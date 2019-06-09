e02e3050df1f281a2f393fa1bbb462a28326bdad
Jay Li <jayli3n@gmail.com>
2019-06-10 00:11:53 +0800

    fb login redirects

---
+++ reducers/authReducer.js
@@ -0,0 +1,23 @@
+import {
+	FACEBOOK_LOGIN_SUCCESS,
+	FACEBOOK_LOGIN_FAIL
+} from '../actions/types';
+
+const INITIAL_STATE = {
+	token: null
+}
+
+export default (state=INITIAL_STATE, action) => {
+	switch (action.type) {
+		case FACEBOOK_LOGIN_SUCCESS:
+			return {
+				token : action.payload
+			}
+		case FACEBOOK_LOGIN_FAIL:
+			return {
+				token : null
+			}
+		default:
+			return state;
+	}
+}
--- screens/auth.js
+++ screens/auth.js
@@ -8,20 +8,33 @@ import {
 class AuthScreen extends Component {
 	componentDidMount() {
 		this.props.facebookLogin();
+		this.onAuthComplete(this.props);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.onAuthComplete(nextProps);
+	}
+
+	onAuthComplete(props) {
+		if(props.token) {
+			this.props.navigation.navigate('main');
+		}
 	}
 
 	render() {
 		return (
 			<View>
-				<Text>Auth Screen</Text>
-				<Text>Auth Screen</Text>
-				<Text>Auth Screen</Text>
-				<Text>Auth Screen</Text>
-				<Text>Auth Screen</Text>
-				<Text>Auth Screen</Text>
+				<Text>Please Login through Facebook</Text>
 			</View>
 		)
 	}
 }
 
-export default connect(null, { facebookLogin })(AuthScreen);
+const mapStateToProps = (state) => {
+	const { auth } = state;
+	return {
+		token: auth.token
+	};
+}
+
+export default connect(mapStateToProps, { facebookLogin })(AuthScreen);
